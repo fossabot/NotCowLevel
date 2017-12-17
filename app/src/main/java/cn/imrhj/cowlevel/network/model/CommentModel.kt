8cea48769024efa28d6e947d2aef20abf71a042a
任洪建 <renhongjian@117sport.com>
2017-12-17 13:48:45 +0800

    添加link类型

---
+++ app/src/main/java/cn/imrhj/cowlevel/network/model/CommentModel.kt
@@ -0,0 +1,21 @@
+package cn.imrhj.cowlevel.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CommentModel(
+
+        @field:SerializedName("comment_count")
+        val commentCount: Int? = null,
+
+        @field:SerializedName("neat_content")
+        val neatContent: ContentModel? = null,
+
+        @field:SerializedName("id")
+        val id: Int? = null,
+
+        @field:SerializedName("title")
+        val title: String? = null,
+
+        @field:SerializedName("content")
+        val content: String? = null
+)
