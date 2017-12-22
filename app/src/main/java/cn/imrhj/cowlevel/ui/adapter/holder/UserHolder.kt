4527e17212a57d4ac6e41ae7f285529658685037
任洪建 <renhongjian@117sport.com>
2017-12-22 17:23:24 +0800

    接入数据

--- app/src/main/java/cn/imrhj/cowlevel/ui/adapter/holder/UserHolder.kt
+++ app/src/main/java/cn/imrhj/cowlevel/ui/adapter/holder/UserHolder.kt
@@ -1,6 +1,11 @@
 package cn.imrhj.cowlevel.ui.adapter.holder
 
+import android.text.Html
+import android.view.View
+import android.widget.TextView
+import cn.imrhj.cowlevel.R
 import cn.imrhj.cowlevel.network.model.UserModel
+import cn.imrhj.cowlevel.utils.StringUtils
 import com.chad.library.adapter.base.BaseViewHolder
 
 /**
@@ -8,6 +13,24 @@ import com.chad.library.adapter.base.BaseViewHolder
  */
 class UserHolder() {
     fun renderHeader(helper: BaseViewHolder, userModel: UserModel) {
+        helper.setText(R.id.tv_name, userModel.name)
+        setOrHideText(helper.getView(R.id.tv_intro), userModel.intro)
+        helper.setText(R.id.tv_vote, "获得 ${userModel.vote_count} 赞")
+        helper.setText(R.id.tv_follower, "关注者 ${userModel.follower_count}")
+        setOrHideText(helper.getView(R.id.tv_bio), userModel.user_bio)
+        helper.setText(R.id.tv_played, userModel.played_count.toString())
+        helper.setText(R.id.tv_comment, userModel.comment_count.toString())
+        helper.setText(R.id.tv_answer, userModel.answer_count.toString())
+        helper.setText(R.id.tv_question, userModel.question_count.toString())
+        helper.setText(R.id.tv_article, userModel.article_count.toString())
+        helper.setText(R.id.tv_collection, userModel.collection_count.toString())
+    }
 
+    private fun setOrHideText(view: TextView, text: String?) {
+        if (StringUtils.isBlank(text)) {
+            view.visibility = View.GONE
+        } else {
+            view.text = Html.fromHtml(text)
+        }
     }
 }
