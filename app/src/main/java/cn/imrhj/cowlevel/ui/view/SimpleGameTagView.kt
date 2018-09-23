d18c0150edd0f1d439b8bcf3dc1ae076fc27da27
任洪建 <hexrhj@gmail.com>
2018-09-23 20:55:44 +0800

    game tag comp

---
+++ app/src/main/java/cn/imrhj/cowlevel/ui/view/SimpleGameTagView.kt
@@ -0,0 +1,17 @@
+package cn.imrhj.cowlevel.ui.view
+
+import android.content.Context
+import android.widget.FrameLayout
+import cn.imrhj.cowlevel.network.model.common.TagModel
+
+class SimpleGameTagView(context: Context, tag: TagModel) : FrameLayout(context) {
+    private val mTag = tag
+    //    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+//    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+//    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+    init {
+
+    }
+
+
+}
