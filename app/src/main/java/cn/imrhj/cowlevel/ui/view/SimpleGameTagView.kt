6a8acb7e93eaf44c800d05335c7a092ddd1f6b5a
任洪建 <hexrhj@gmail.com>
2018-09-26 07:42:09 +0800

    update studio

--- app/src/main/java/cn/imrhj/cowlevel/ui/view/SimpleGameTagView.kt
+++ app/src/main/java/cn/imrhj/cowlevel/ui/view/SimpleGameTagView.kt
@@ -2,15 +2,18 @@ package cn.imrhj.cowlevel.ui.view
 
 import android.content.Context
 import android.widget.FrameLayout
+import android.widget.TextView
 import cn.imrhj.cowlevel.network.model.common.TagModel
 
 class SimpleGameTagView(context: Context, tag: TagModel) : FrameLayout(context) {
     private val mTag = tag
+
     //    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 //    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 //    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
     init {
-
+        val textView = TextView(context)
+        textView.text = mTag.name
     }
 
 
